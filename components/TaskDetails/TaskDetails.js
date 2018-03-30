9967722c158baf63695e0495bed562d28eba363e
Brandon Allred <b32alls@gmail.com>
2018-03-30 10:22:18 -0600

    changes to the checklist items on task details

--- components/TaskDetails/TaskDetails.js
+++ components/TaskDetails/TaskDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, TouchableHighlight } from 'react-native';
-import { Container, Header, Left, Body, Right, Button, Title, Text, Input, Item, Content, Form, Footer, View } from 'native-base';
+import { Container, Header, CheckBox, Left, Body, Right, Button, Title, Text, Input, Item, Content, Form, Footer, View } from 'native-base';
 import TaskDatePicker from './TaskDatePicker.js';
 import DurationPicker from './DurationPicker'
 import axios from 'axios';
@@ -19,7 +19,7 @@ export default class TaskDetails extends Component {
             duedate: '',
             label: '',
             members: [],
-            checklistItems: [''],
+            checklistItems: ['hello'],
             newChecklistItem: '',
             activity: [],
             name: '',
@@ -32,7 +32,8 @@ export default class TaskDetails extends Component {
             hours: '00',
             minutes: '00',
             milliseconds: 0,
-            durationModalVisable: false
+            durationModalVisable: false,
+            checked: false
         }
         this.handleLabelColor = this.handleLabelColor.bind(this)
         this.selectDate = this.selectDate.bind(this)
@@ -48,11 +49,14 @@ export default class TaskDetails extends Component {
         console.log(item)
         this.setState({ newChecklistItem: [item.item] })
     }
+    completeChecklistItem(e) {
+        console.log(e)
+    }
     addChecklistItem(e) {
         // add an axios call to get all checklist items by id from the database
         console.log(e)
         const { checklistItems, newChecklistItem } = this.state
-        this.setState({ checklistItems: newChecklistItem, newChecklistItem: '' })
+        this.setState({ checklistItems: newChecklistItem, newChecklistItem: '' });
         // axios.put('/api/checklistItem').then(res=>{
         //     console.log(res)
         //     this.setState({checklistItems:res.data})
@@ -94,11 +98,21 @@ export default class TaskDetails extends Component {
         console.log(this.props, "these are the props")
         const { padding, margin, separate, inputSize, header, inputColor, inputRight, inputBox_1, header_top, header_bottom, createChecklist, Label, addItemMargin, userInitialStyle, activityContent, iconSize, commentStyle, labelStyle } = styles
         let checklist = this.state.checklistItems.map((item, i) => {
-            return (
+            // if (this.state.checked === false){
+            //     return (
+            //         <Item key={i}>
+            //             <CheckBox checked={false} style={{ borderColor: 'gray', borderRadius: 0, marginRight: 20, height: 15, width: 15, backgroundColor:'transparent' }} /><Text>{item}</Text>
+            //             <Text placeholder='Add item...' style={[padding, addItemMargin, inputSize]} onChangeText={(item) => this.handleChecklistItem({ id: i, item })} onEndEditing={(e) => this.addChecklistItem(e)} />
+            //         </Item>
+            //     )
+            // }
+            // else {
+                return (
                 <Item key={i}>
-                    <Input placeholder='Add item...' style={[padding, addItemMargin, inputSize]} onChangeText={(item) => this.handleChecklistItem({ id: i, item })} onEndEditing={(e) => this.addChecklistItem(e)} />
-                </Item>
-            )
+                    <CheckBox checked={true} style={{ borderColor: 'gray', borderRadius: 0, marginRight: 20, height: 15, width: 15, backgroundColor:'transparent', fontColor:'gray' }} /><Text>{item}</Text>
+                    <Text placeholder='Add item...' style={[padding, addItemMargin, inputSize]} onChangeText={(item) => this.handleChecklistItem({ id: i, item })} onEndEditing={(e) => this.addChecklistItem(e)} />
+                </Item>)
+            // }
         })
         return (
             <View style={{ marginTop: 22 }}>
@@ -131,11 +145,11 @@ export default class TaskDetails extends Component {
                         <Item regular style={inputBox_1} >
                             <Input placeholder='Tap to add a description' onChangeText={(description) => this.setState({ description })} />
                         </Item>
-                        <Item style={[inputSize, margin]}>
+                        <Item style={[inputSize, margin, { justifyContent: 'space-between' }]}>
                             <IconF active name='clock' size={15} />
                             {/* <Input placeholder='Due date...' placeholderTextColor={'black'}/> */}
                             <TaskDatePicker selectDate={this.selectDate} date={this.state.date} />
-                            <DurationPicker 
+                            <DurationPicker
                                 showDurationPicker={this.state.durationModalVisable}
                                 saveDuration={this.saveDuration}
                                 cancelDuration={this.cancelDuration}
@@ -144,15 +158,15 @@ export default class TaskDetails extends Component {
                                 onPress={() => {
                                     this.setModalVisible(!this.state.LabelModalVisable);
                                 }}>
-                                <Text style={{color:'#585858', fontSize:17}}>  <IconE name='time-slot' size={15} color={'#303030'}/>  Duration</Text>
+                                <Text style={{ color: '#585858', fontSize: 17 }}>  <IconE name='time-slot' size={15} color={'#303030'} />  Duration</Text>
                             </TouchableHighlight>
                         </Item>
                         <Item style={labelStyle}>
                             <IconSLI active name='tag' size={15} />
-                            <Labels 
-                            labelColor={this.handleLabelColor} 
-                            color={this.state.color} 
-                            isVisable={this.state.LabelModalVisable} />
+                            <Labels
+                                labelColor={this.handleLabelColor}
+                                color={this.state.color}
+                                isVisable={this.state.LabelModalVisable} />
                             {this.state.color !== '' ? <View style={{ height: 10, width: 25, backgroundColor: this.state.color, justifyContent: 'flex-end', marginLeft: 230 }} /> : null}
                         </Item>
                         <Item style={margin}>
@@ -164,16 +178,19 @@ export default class TaskDetails extends Component {
                             <Input disabled placeholder='Checklist...' style={[inputSize]} name='checklist' onChangeText={(text) => this.handleChecklistName({ id: i, name: text })} />
                         </Item>
                         {checklist}
+                        <Item>
+                            <Input placeholder='Add item...' style={[padding, addItemMargin, inputSize]} onChangeText={(item) => this.handleChecklistItem({ item })} onEndEditing={(e) => this.addChecklistItem(e)} />
+                        </Item>
                         <Item disabled style={separate}>
                             <IconF name='activity' style={[iconSize, padding]} />
-                            <Input placeholder='Activity' style={[padding, inputSize]} placeholder='Activity' />
+                            <Input disabled placeholder='Activity' style={[padding, inputSize]} placeholder='Activity' />
                             <IconSLI name='settings' style={{ paddingRight: 15 }} size={15} />
                         </Item>
                     </Content>
                     <Content style={activityContent}>
                         <Item>
                             <View style={userInitialStyle}><Text><Text>{this.state.user.split(' ').map((item, i) => { if (i <= 1) return item[0] }).join('')}</Text></Text></View>
-                            <Input placeholder='Add a comment...' style={ commentStyle } onChangeText={(text) => this.addComment(text)} />
+                            <Input placeholder='Add a comment...' style={commentStyle} onChangeText={(text) => this.addComment(text)} />
                         </Item>
                     </Content>
                 </Modal>
@@ -187,7 +204,8 @@ const styles = ({
     },
     margin: {
         paddingLeft: 10,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        paddingRight: 20
     },
     addItemMargin: {
         marginLeft: 10
@@ -245,16 +263,16 @@ const styles = ({
         bottom: 10,
         flexDirection: 'row'
     },
-    commentStyle:{
-        borderBottomColor: 'transparent', 
+    commentStyle: {
+        borderBottomColor: 'transparent',
         justifyContent: 'flex-start'
     },
-    labelStyle:{
-        height: 40, 
-        alignContent: 'center', 
-        backgroundColor: '#fff', 
-        paddingLeft: 10, 
-        flex: 1, 
+    labelStyle: {
+        height: 40,
+        alignContent: 'center',
+        backgroundColor: '#fff',
+        paddingLeft: 10,
+        flex: 1,
         alignItems: 'center'
     }
 });
