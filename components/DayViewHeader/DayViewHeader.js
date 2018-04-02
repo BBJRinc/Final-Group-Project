5507d8b7a9729b25412d879129f754acca5d7b7a
Ross Dunford <dunford@Rus-MacBook.local>
2018-04-02 09:49:24 -0600

    DayViewHeader Component and getting updates

--- components/DayViewHeader/DayViewHeader.js
+++ components/DayViewHeader/DayViewHeader.js
@@ -1,19 +1,39 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { View, Image, StyleSheet, ImageBackground, TouchableHighlight } from "react-native";
-import { Container,  Separator, Content, Icon, Header, ListIcon, Footer, Button, Text, List, Left, Right, Body, ListItem, } from 'native-base';
+import { Container, Separator, Content, Icon, Header, ListIcon, Footer, Button, Text, List, Left, Right, Body, ListItem, } from 'native-base';
 import moment from 'moment';
 import IconF from 'react-native-vector-icons/Feather';
 import IconEV from 'react-native-vector-icons/EvilIcons';
 
-export default class DayViewHeader extends Component{
-    render(){
-        return(
+moment()
+
+export default class DayViewHeader extends Component {
+    render() {
+        return (
             <Container>
 
                 <Header>
+
                     <Left Icon>
-                    <IconF name="chevron-left" color="#000000" size={15}/>
+                        <IconF name="chevron-left"
+                            color="#000000"
+                            size={15}
+                            onPress={() => { }}
+                        />
                     </Left>
+
+                    <Body>
+                        
+                    </Body>
+
+                    <Right>
+                        <IconF
+                            name="chevron-right"
+                            color="#000000"
+                            size={15}
+                            onPress={() => { }}
+                        />
+                    </Right>
 
                 </Header>
 
